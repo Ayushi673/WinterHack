166f367e547c97ee89399db697131937c05f868c
Ayushi673 <dasayushi16@gmail.com>
2022-01-16 16:35:20 +0530

    styling

--- frontend/web.mql.todo/src/components/Home.js
+++ frontend/web.mql.todo/src/components/Home.js
@@ -2,6 +2,15 @@ import './App.css';
 import { useState } from 'react';
 import Search from './Search.js';
 
+const styles={
+    inputStyle:{
+        width: "50%",
+        height: "40px",
+        borderRadius: "25px",
+        padding: "1px 12px"
+    }
+}
+
 export default function Home() {
 
     const [searchInput, setSearchInput] = useState('a');
@@ -9,8 +18,10 @@ export default function Home() {
     return(
         <div>
         <div className="App-header">
-            <p>Search for hotels:</p>
+            <p>Hi!</p>
+            <p><b>Discover your next best "STAY"cation right here, right now!</b></p>
             <input type="text"
+            style={styles.inputStyle}
             value={searchInput}
             placeholder="Enter preference"
             onChange={(e) => {
